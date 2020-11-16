ea2d54a5eb8d70d6c9f129d26407b346802df7d4
ricky_saurav <luckyricky007@gmail.com>
2020-11-16 13:51:26 +0530

    Fix pyright installer

--- lua/lspconfig/pyright_ls.lua
+++ lua/lspconfig/pyright_ls.lua
@@ -1,21 +1,19 @@
 local configs = require 'lspconfig/configs'
 local util = require 'lspconfig/util'
 
-local server_name = "pyright"
+local server_name = "pyright_ls"
+local package_name = "pyright"
 local bin_name = "pyright-langserver"
-if vim.fn.has('win32') == 1 then
-  bin_name = bin_name..".cmd"
-end
 
 local installer = util.npm_installer {
   server_name = server_name;
-  packages = {server_name};
+  packages = {package_name};
   binaries = {bin_name};
 }
 
 configs[server_name] = {
   default_config = {
-    cmd = {bin_name, "--stdio"};
+    cmd = {bin_name , "--stdio"};
     filetypes = {"python"};
     root_dir = util.root_pattern(".git", "setup.py",  "setup.cfg", "pyproject.toml", "requirements.txt");
     settings = {
@@ -27,6 +25,17 @@ configs[server_name] = {
       };
     };
    };
+  on_new_config = function(new_config)
+    local install_info = installer.info()
+    if install_info.is_installed then
+      if type(new_config.cmd) == 'table' then
+        -- Try to preserve any additional args from upstream changes.
+        new_config.cmd[1] = install_info.binaries[bin_name]
+      else
+        new_config.cmd = {install_info.binaries[bin_name]}
+      end
+    end
+  end;
   docs = {
     description = [[
 https://github.com/microsoft/pyright
